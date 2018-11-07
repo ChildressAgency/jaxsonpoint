e06a29bce9838d3739a527466537bafeaf9943fc
halfg33k <johnsonam17@gmail.com>
2018-11-07 15:46:58 -0500

    hero

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<?php 
+$hero = get_field( 'hero' );
+if( $hero ): ?>
+<div class="hero" style="background-image: url(<?php echo $hero['background']; ?>)">
+    <div class="hero__content">
+        <?php if( $hero['text'] ): ?><h2 class="hero__text"><?php echo $hero['text']; ?></h2><?php endif; ?>
+        <?php if( $hero['link'] ): ?><a href="<?php echo $hero['link']; ?>" class="btn btn--primary"><?php echo $hero['link_text']; ?></a><?php endif; ?>
+    </div>
+</div>
+<?php endif; ?>
+
+<?php get_footer(); ?>
