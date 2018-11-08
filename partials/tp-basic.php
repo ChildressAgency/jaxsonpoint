983f17b116c1c616354e20ee15c1231938791809
halfg33k <johnsonam17@gmail.com>
2018-11-08 12:03:25 -0500

    added section heading stuff

--- partials/tp-basic.php
+++ partials/tp-basic.php
@@ -1,5 +1,8 @@
 <div class="section">
     <div class="container<?php if( get_sub_field( 'is_thin' ) ) echo ' container--thin'; ?>">
+        <?php if( get_sub_field( 'section_heading' ) ): ?>
+            <h2 class="section-heading"><?php the_sub_field( 'section_heading' ); ?></h2>
+        <?php endif; ?>
         <?php echo get_sub_field( 'content' ); ?>
     </div>
 </div>
