5732c662b3bca7a870c632767a21e9c253db91eb
halfg33k <johnsonam17@gmail.com>
2018-11-08 09:28:34 -0500

    home page changes

--- front-page.php
+++ front-page.php
@@ -6,7 +6,6 @@ if( $hero ): ?>
 <div class="hero" style="background-image: url(<?php echo $hero['background']; ?>)">
     <div class="hero__content">
         <?php if( $hero['text'] ): ?><h2 class="hero__text"><?php echo $hero['text']; ?></h2><?php endif; ?>
-        <?php if( $hero['link'] ): ?><a href="<?php echo $hero['link']; ?>" class="btn btn--primary"><?php echo $hero['link_text']; ?></a><?php endif; ?>
     </div>
 </div>
 <?php endif; ?>
