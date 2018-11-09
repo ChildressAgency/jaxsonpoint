ad175c156cb8e38e6f1a0bf05655418c57e2801b
halfg33k <johnsonam17@gmail.com>
2018-11-09 10:15:14 -0500

    added background option

--- partials/tp-basic.php
+++ partials/tp-basic.php
@@ -1,4 +1,5 @@
-<div class="section">
+<div class="section" <?php if( get_sub_field( 'background' ) ): ?>style="background-image: url(<?php the_sub_field( 'background' ); ?>);<?php endif; ?>">
+    <?php if( get_sub_field( 'background' ) ): ?><div class="white-gradient-overlay"></div><?php endif; ?>
     <div class="container<?php if( get_sub_field( 'is_thin' ) ) echo ' container--thin'; ?>">
         <?php if( get_sub_field( 'section_heading' ) ): ?>
             <h2 class="section-heading"><?php the_sub_field( 'section_heading' ); ?></h2>
