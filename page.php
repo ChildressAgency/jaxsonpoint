60d22802bb2e36980c6ded277ecf306dd4e2ec4f
halfg33k <johnsonam17@gmail.com>
2018-11-09 11:43:18 -0500

    buildable page

--- page.php
+++ page.php
@@ -1,12 +1,16 @@
 <?php get_header(); ?>
-    <section class="content">
-      <div class="container">
-        <?php if(have_posts()): while(have_posts()): the_post(); ?>
-          <h1 class="page-title"><?php the_title(); ?></h1>
-          <article>
-            <?php the_content(); ?>
-          </article>
-        <?php endwhile; endif; ?>
-      </div>
-    </section>
+
+<div class="hero" style="background-image: url(<?php the_field( 'hero_image' ); ?>)">
+    <div class="hero__content">
+        <?php 
+        $hero_text = get_field( 'hero_text' );
+        if( !$hero_text )
+            $hero_text = strtoupper( get_the_title() );
+        ?>
+        <h2 class="hero__text"><?php echo $hero_text ?></h2>
+    </div>
+</div>
+
+<?php get_template_part( 'partials/tp-flexible' ); ?>
+
 <?php get_footer(); ?>
