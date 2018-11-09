4762730a00d200fcb04726c8f6e9eb1b4a488496
halfg33k <johnsonam17@gmail.com>
2018-11-09 11:42:48 -0500

    hero is different for home page

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 <?php 
 $hero = get_field( 'hero' );
 if( $hero ): ?>
-<div class="hero" style="background-image: url(<?php echo $hero['background']; ?>)">
+<div class="hero hero--home" style="background-image: url(<?php echo $hero['background']; ?>)">
     <div class="hero__content">
         <?php if( $hero['text'] ): ?><h2 class="hero__text"><?php echo $hero['text']; ?></h2><?php endif; ?>
     </div>
