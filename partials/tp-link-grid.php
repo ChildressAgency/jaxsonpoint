fe120f6ae1ca8f748dad1c6b70aa8e6c9fa1345b
halfg33k <johnsonam17@gmail.com>
2018-11-09 16:49:57 -0500

    using heading style elsewhere

--- partials/tp-link-grid.php
+++ partials/tp-link-grid.php
@@ -1,6 +1,6 @@
 <div class="section">
     <div class="container<?php if( get_sub_field( 'is_thin' ) ) echo ' container--thin'; ?>">
-        <h2 class="section-heading link-grid__heading"><?php the_sub_field( 'heading' ); ?></h2>
+        <h2 class="section-heading section-heading--alt"><?php the_sub_field( 'heading' ); ?></h2>
         <div class="link-grid">
             <?php if( have_rows( 'links' ) ): $i = 0; while( have_rows( 'links' ) ): the_row(); ?>
                 <a href="<?php the_sub_field( 'link' ); ?>" class="link-grid__item<?php if( $i == 1 ) echo ' link-grid__item--tall'; ?>">
